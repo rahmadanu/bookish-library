64968d22574111f9fcac2c37590416de6af0c58f
rahmadanu <sadewarahmadanu@gmail.com>
2023-10-07 00:43:57 +0700

    Initial commit

---
+++ app/src/main/java/com/hepipat/bookish/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hepipat.bookish
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
